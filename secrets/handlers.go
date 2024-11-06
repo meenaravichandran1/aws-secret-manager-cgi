6a405cc4e2c26b0b946ef710e98baa8db139e779
Meena Ravichandran <meena.ravichandran@harness.io>
2024-11-06 11:18:46 -0500

    send err response with err

--- secrets/handlers.go
+++ secrets/handlers.go
@@ -37,22 +37,25 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 
 	switch operation {
 	case "connect":
-		result, _ = secretManager.Connect(ctx, in.SecretParams.Secret.Name)
+		result, err = secretManager.Connect(ctx, in.SecretParams.Secret.Name)
 	case "validate_ref":
-		result, _ = secretManager.ValidateReference(ctx, in.SecretParams.Secret.Name)
+		result, err = secretManager.ValidateReference(ctx, in.SecretParams.Secret.Name)
 	case "fetch":
-		result, _ = secretManager.FetchSecret(ctx, *in.SecretParams.Secret)
+		result, err = secretManager.FetchSecret(ctx, *in.SecretParams.Secret)
 	case "create":
-		result, _ = secretManager.UpsertSecret(ctx, *in.SecretParams.Secret, nil)
+		result, err = secretManager.UpsertSecret(ctx, *in.SecretParams.Secret, nil)
 	case "update":
-		result, _ = secretManager.UpsertSecret(ctx, *in.SecretParams.Secret, in.SecretParams.ExistingSecret)
+		result, err = secretManager.UpsertSecret(ctx, *in.SecretParams.Secret, in.SecretParams.ExistingSecret)
 	case "rename":
-		result, _ = secretManager.RenameSecret(ctx, *in.SecretParams.Secret, in.SecretParams.ExistingSecret)
+		result, err = secretManager.RenameSecret(ctx, *in.SecretParams.Secret, in.SecretParams.ExistingSecret)
 	case "delete":
-		result, _ = secretManager.DeleteSecret(ctx, *in.SecretParams.Secret)
+		result, err = secretManager.DeleteSecret(ctx, *in.SecretParams.Secret)
 	default:
 		SendErrorResponse(w, errors.New("invalid action"), fmt.Sprintf("The specified action %s is not supported", operation), http.StatusBadRequest)
 		return
+	}
+	if err != nil {
+		SendErrorResponse(w, err, "Failed to do AWS secret operation", http.StatusBadRequest)
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(result)
